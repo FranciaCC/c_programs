cae6ca572722bb169755dc38d98d68b9604e0299
Jorge Atempa <atempaitm@gmail.com>
2020-03-01 17:49:27 -0800

    Implicit Type Conversion

--- demo.c
+++ demo.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-  int a = 0;
-  int b = 0;
+  int a = 2;
+  double b = 3.5;
+  double c = a * b;
+  double d = a / b;
+  int e = a * b;
+  int f = a / b;
 
-  printf("a=%d, b=%d\n", a, b);
-
-  a++;
-  b--;
-
-  printf("a=%d, b=%d\n", a, b);
+  printf("a=%d, b=%.3f, c=%.3f, d=%.3f, e=%d, f=%d\n",
+    a, b, c, d, e, f);
 
   return 0;
 }
