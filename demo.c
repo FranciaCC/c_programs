7c440e717491b250cecc375c901ba7caacfc717e
Jorge Atempa <atempaitm@gmail.com>
2020-03-01 17:31:46 -0800

    Print out the size of some basic C types

--- demo.c
+++ demo.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main() {
-  printf("hello world!\n");
+int main(int argc, char *argv[]) {
+  // Print out the size of some basic C types
+  printf("a char is %ld bytes\n", sizeof(char));
+  printf("an int is %ld bytes\n", sizeof(int));
+  printf("an float is %ld bytes\n", sizeof(float));
+  printf("a double is %ld bytes\n", sizeof(double));
+  printf("a short int is %ld bytes\n", sizeof(short int));
+  printf("a long int is %ld bytes\n", sizeof(long int));
+  printf("a long double is %ld bytes\n", sizeof(long double));
+
   return 0;
 }
